c8d1e5daccfe0a04450d9b3c77e08c96d5607137
Lakshya Pandey <74096902+lakshyapandey1250@users.noreply.github.com>
2020-12-24 02:24:16 +0530

    Made The Code More Readable

--- Dictionary.py
+++ Dictionary.py
@@ -42,29 +42,29 @@ root.title("Oxford Dictionary")
 root.geometry(f"{width}x{height}")
 
 # Initializing And Setting The Images
-logo_image = tk.PhotoImage(file="Logo.png")
-background_image = tk.PhotoImage(file='Background.png')
+logo_image = tk.PhotoImage(file = "Logo.png")
+background_image = tk.PhotoImage(file = 'Background.png')
 root.iconphoto(False, logo_image)
 root.iconbitmap("Icon.ico")
-background_label = tk.Label(root, image=background_image)
-background_label.place(relwidth=1, relheight=1)
+background_label = tk.Label(root, image = background_image)
+background_label.place(relwidth = 1, relheight = 1)
 
 # Creating The Main Window
-frame1 = tk.Frame(root, bg=background_color, bd=5)
-frame1.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+frame1 = tk.Frame(root, bg = background_color, relief = RIDGE, bd = 5)
+frame1.place(relx = 0.5, rely = 0.1, relwidth = 0.75, relheight = 0.1, anchor = 'n')
 
-entry1 = tk.Entry(frame1, font=("Times New Roman", 20))
-entry1.place(relwidth=0.67, relheight=1)
+entry1 = tk.Entry(frame1, font = ("Times New Roman", 20), relief = RIDGE)
+entry1.place(relx = 0.02, rely = 0.1, relwidth = 0.6, relheight = 0.8)
 
-button1 = tk.Button(frame1, text="Get Definiton", font=("Times New Roman", 20), command=lambda:get_define(entry.get()), cursor="hand2")
-button1.place(relx=0.69, relheight=1, relwidth=0.3)
+button1 = tk.Button(frame1, text = "Get Definiton", font = ("Times New Roman", 20), relief = RIDGE, command = lambda:get_define(entry.get()), cursor = "hand2")
+button1.place(relx = 0.68, rely = 0.1, relwidth = 0.3, relheight = 0.8)
 
-frame2 = tk.Frame(root, bg=background_color, bd=10)
-frame2.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
+frame2 = tk.Frame(root, bg = background_color, relief = RIDGE, bd = 10)
+frame2.place(relx = 0.5, rely = 0.25, relwidth = 0.75, relheight = 0.6, anchor = 'n')
 
-label1 = tk.Label(frame2, font=("Courier",20))
-label1.place(relwidth=1, relheight=1)
+label1 = tk.Label(frame2, font = ("Courier", 20), relief = RIDGE)
+label1.place(relwidth = 1, relheight = 1)
 
 # ----- Driver Code -----
-if __name__ == "__main__":
+if __name__ =  = "__main__":
 	root.mainloop()
